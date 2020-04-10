226ce4d6295a005cc070682233f7d0344772ab66
Pierreauger <pierre.auger@isen-ouest.yncrea.fr>
2020-04-10 19:55:37 +0200

    bjbk

--- accesdomelmnt.js
+++ accesdomelmnt.js
@@ -1,19 +1,18 @@
 
 let titre = document.title;
-console.log(titre);
-let typepage;
-console.log(typepage);
+console.log("==================================");
+console.log("titre de la page :" +titre);
+console.log("type du cntenu de la page:" +document.body);
 let i = document.links;
-console.log(i[2].href);
+console.log("3ème bande annonce :" +i[2].href);
 let npg = document.getElementsByTagName("p");
-console.log("Paragraphes");
-console.log(npg.length);
-let img = document.images;
-console.log("Images");
-console.log(img.length);
-let sy = document.getElementsByClassName("c1");
-console.log("Synopsis");
-console.log(sy.length);
-let date = document.getElementById("2014");
-console.log(date.innerHTML);
-let 
+console.log(" Le titre GONE GIRL est de type :" +document.head);
+console.log("====================================");
+console.log("Nombre de paragraphes du doc : " +npg.length +" Et d'Images :" +document.images.length);
+console.log("====================================");
+console.log("On a le nombre de Synopsis : " +document.getElementsByClassName("c1").length);
+console.log("Première rubrique des films en l'année : " +document.getElementById("2014").innerHTML);
+console.log("Nombre de film sous la rubrique 2016 : " );
+console.log("====================================");
+console.log("synopsis du film : " );
+console.log(+)
