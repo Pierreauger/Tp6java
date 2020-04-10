4ad45e6d1bb4cf1e65b6388784f375fafcd57f8c
Pierreauger <pierre.auger@isen-ouest.yncrea.fr>
2020-04-10 17:07:08 +0200

    dsf

---
+++ accesdomelmnt.js
@@ -0,0 +1,19 @@
+
+let titre = document.title;
+console.log(titre);
+let typepage;
+console.log(typepage);
+let i = document.links;
+console.log(i[2].href);
+let npg = document.getElementsByTagName("p");
+console.log("Paragraphes");
+console.log(npg.length);
+let img = document.images;
+console.log("Images");
+console.log(img.length);
+let sy = document.getElementsByClassName("c1");
+console.log("Synopsis");
+console.log(sy.length);
+let date = document.getElementById("2014");
+console.log(date.innerHTML);
+let 
